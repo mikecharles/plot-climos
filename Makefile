db1e9ceea7c83a107a1f67ba946b39cb3c8af6ec
Mike Charles <mike.charles@noaa.gov>
2016-10-05 17:25:48 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,56 @@
+.PHONY: clean-pyc clean-build docs clean
+define BROWSER_PYSCRIPT
+import os, webbrowser, sys
+try:
+	from urllib import pathname2url
+except:
+	from urllib.request import pathname2url
+
+webbrowser.open("file://" + pathname2url(os.path.abspath(sys.argv[1])))
+endef
+export BROWSER_PYSCRIPT
+BROWSER := python -c "$$BROWSER_PYSCRIPT"
+
+help:
+	@echo "clean - remove all build, test, coverage and Python artifacts"
+	@echo "clean-build - remove build artifacts"
+	@echo "clean-pyc - remove Python file artifacts"
+	@echo "clean-test - remove test and coverage artifacts"
+	@echo "lint - check style with flake8"
+	@echo "test - run tests quickly with the default Python"
+	@echo "coverage - check code coverage quickly with the default Python"
+	@echo "docs - generate local API HTML documentation using MkDocs"
+	@echo "github-docs - generate API HTML documentation on GitHub.io using MkDocs"
+	@echo "release - package and upload a release"
+	@echo "dist - package"
+	@echo "install - install the package to the active Python's site-packages"
+
+clean: clean-build clean-pyc clean-test
+
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr .eggs/
+	rm -rf docs/api
+	find . -name '*.egg-info' -exec rm -fr {} +
+	find . -name '*.egg' -exec rm -f {} +
+	rm -rf site/
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+
+clean-test:
+	rm -rf .cache
+	rm -f .coverage
+	rm -fr htmlcov/
+	rm -rf cov-report/
+
+lint:
+	flake8 cpc.geoplot tests
+
+install: clean
+	conda install --yes --file conda-requirements.txt
+	pip install -r pip-requirements.txt
---
+++ plot_climos.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+# Built-in
+import sys
+import os
+import warnings
+from datetime import date
+
+# Third-party
+import jinja2
+import pandas as pd
+import numpy as np
+from cpc.geogrids import Geogrid
+from cpc.geoplot import Field, Map
+from PIL import Image
+
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+# --------------------------------------------------------------------------------------------------
+# Define script usage
+#
+usage = """Usage: {} <CLIMO-TMPL>
+  where:
+    CLIMO-TMPL  template of climatology files - any $VAR strings (eg. $HOME) will be
+                replaced with the corresponding environment variable, and {{mmdd}}
+                will be replaced with the value of mmdd in a loop over days of the
+                year in this script
+    OUT-DIR     directory to write output image files to
+""".format(os.path.basename(__file__))
+
+# --------------------------------------------------------------------------------------------------
+# Get command-line args
+#
+if len(sys.argv)-1 < 2:
+    print(usage)
+    exit(1)
+else:
+    climo_tmpl_str = sys.argv[1]
+    out_dir = sys.argv[2]
+
+# --------------------------------------------------------------------------------------------------
+# Make a Jinja2 template from the climo file template
+#
+climo_templ = jinja2.Template(os.path.expanduser(os.path.expandvars(climo_tmpl_str)))
+
+# --------------------------------------------------------------------------------------------------
+# Create a Geogrid for plotting data
+#
+geogrid = Geogrid('1deg-global')
+
+# --------------------------------------------------------------------------------------------------
+# Loop over mmdd's (including 2/29) and create a plot for each day
+#
+for mmdd in pd.date_range('20000101', '20001231').strftime('%m%d'):
+    # Render Jinja2 climo file template
+    climo_file = climo_templ.render({'mmdd': mmdd})
+    # Read in climo data
+    data = np.fromfile(climo_file, 'float32')
+    # Remove missing values
+    data = np.where(data == -999, np.nan, data)
+    # Remove < 33.333 (don't want to plot these values at all)
+    data = np.where((~np.isnan(data)) & (data < 33.333), np.nan, data)
+    # Create a Map
+    title_date = date(2000, int(mmdd[0:2]), int(mmdd[2:4])).strftime('%d %b')
+    title = 'Percent of Years Without Precip - {}'.format(title_date)
+    map = Map(cbar_ends='square', title=title)
+    # Create a Field
+    field = Field(data, geogrid, levels=[0, 33.333, 66.666, 100],
+                  fill_colors=['white', (1, 0.6, 0.16), (0.6, 0.2, .015)])
+    # Plot Field
+    map.plot(field)
+    # Save plot
+    os.makedirs(out_dir)
+    plot_file = '{}/percent-dry-climo-{}.png'.format(out_dir, mmdd)
+    map.save(plot_file, dpi=300)
+    # Reduce file size
+    im = Image.open(plot_file)
+    im.convert('P', palette=Image.ADAPTIVE, colors=256).save(plot_file)
+
+
+
